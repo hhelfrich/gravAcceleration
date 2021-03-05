6f633674b39083571b7f01ea3360c124f5726a61
Halle Helfrich <hallehelfrich@dhcp-172-16-236-237.dyn.pittstate.edu>
2021-03-05 13:33:08 -0600

    updated to work with small particle file

--- gravAcceleration.py
+++ gravAcceleration.py
@@ -5,18 +5,20 @@ import math
 G = 6.67*10**-11
 h = .000002
 particles = []
+file1 = open("particleSmall.txt", "r")
+for line in file1:
+    x, y, z, m = line.split(" ") 
+    x = float(x)
+    y = float(y)
+    z = float(z)
+    m = float(m)
+    #p = particle.Particle(x, y, z, m)
+    particles.append(particle.Particle(x, y, z, m))
+file1.close()
 
 def gravPotential(point, particles):
     phi = 0.0
     file1 = open("particleSmall.txt", "r")
-    for line in file1:
-        x, y, z, m = line.split(" ") 
-        x = float(x)
-        y = float(y)
-        z = float(z)
-        m = float(m)
-        #p = particle.Particle(x, y, z, m)
-        particles.append(particle.Particle(x, y, z, m))
     for p in particles:
         r = math.sqrt((p.x - point[0])**2 + (p.y - point[1])**2 + (p.z - point[2])**2)
         phi += -G*p.m/r
@@ -27,6 +29,7 @@ def centraldifferencegrav3D(f, point, h, i, particles):
     x_2 = [0,0,0]
     for j in range(0,3):
         x_1[j] = point[j]
+        x_2[j] = point[j]
         if (j == i):
             x_1[j] -= h/2
             x_2[j] += h/2
@@ -50,25 +53,22 @@ f_z = 0
 
 f = gravPotential(point, particles)
 i = 0
-f_x = centraldifferencegrav3D(f, point, h, i, particles)
+f_x = centraldifferencegrav3D(gravPotential, point, h, i, particles)
 
 i = 1
-f_y = centraldifferencegrav3D(f, point, h, i, particles)
+f_y = centraldifferencegrav3D(gravPotential, point, h, i, particles)
 
 i = 2
-f_z = centraldifferencegrav3D(f, point, h, i, particles)
+f_z = centraldifferencegrav3D(gravPotential, point, h, i, particles)
 
 gradient = [f_x, f_y, f_z]
 
-masstotal = 0
-for p in particles:
-    masstotal += particles.m
+#masstotal = 0
+#for p in particles:
+    #masstotal += p.m
 
 
 acceleration = [0, 0, 0]
-acceleration = gradient/masstotal
+acceleration = gradient
 
 print(str(acceleration))
-
-
-    
